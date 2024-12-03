9690e1bb78fe76abc6c7b223f35339bee7baf016
TreeAspen <sy4254@nyu.edu>
2024-12-02 19:48:22 -0500

    Final

--- arduino.ino
+++ arduino.ino
@@ -28,35 +28,28 @@ void loop()
 
   if (state == 0 && input2 == 1) {
     state = 1;
-    Serial.println("Step 1: Input 2 correct");
   } 
   else if (state == 1 && input3 == 1) {
     state = 2;
-    Serial.println("Step 2: Input 3 correct");
   }
   else if (state == 2 && input4 == 1) {
     state = 3;
-    Serial.println("Step 3: Input 4 correct");
   }
   else if (state == 3 && input4 == 0) {
     state = 4;
-    Serial.println("Step 4: Input 4 reset to 0");
   }
   else if (state == 4 && input3 == 0) {
     state = 5;
-    Serial.println("Step 5: Input 3 reset to 0");
   }
   else if (state == 5 && input2 == 0) {
     state = 6;
-    Serial.println("Step 6: Input 2 reset to 0");
   }
 
   if (state == 6) {
     digitalWrite(6, HIGH); 
     digitalWrite(7, LOW);
-    Serial.println("Process Complete");
     if(input2 == 1||input3 == 1||input4 == 1) {
-      state ==0;
+      state == 0;
     }
 
   } 
@@ -64,6 +57,5 @@ void loop()
     digitalWrite(6, LOW); 
     digitalWrite(7, HIGH);
   }
-
   delay(500);
 }
